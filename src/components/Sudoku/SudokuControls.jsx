41dfdf3b165e502f74e40d4fb796d1238b795b84
yaroslavz-gtm <yaroslav.z@gtmarketing.ro>
2026-01-03 23:38:39 +0100

    add hotkeys for undo/redo

--- src/components/Sudoku/SudokuControls.jsx
+++ src/components/Sudoku/SudokuControls.jsx
@@ -1,8 +1,16 @@
 import { useHistoryContext } from "../../context/HistoryProvider"
 import { Icon } from "@iconify/react"
+import useGlobalDOMEvents from "../../hooks/useGlobalDOMEvents"
 
 export default function SudokuControls({ updateBoard }) {
     const { undo, redo } = useHistoryContext()
+
+    useGlobalDOMEvents('keydown', (e) => {
+        if (e.ctrlKey) {
+            e.key === 'z' && handleUndo()
+            e.key === 'Z' && handleRedo()
+        }
+    })
 
     const handleUndo = () => updateBoard(undo())
     const handleRedo = () => updateBoard(redo())
